087e8dcf73e51cdf3c868f87dd8452fa1361abd6
Uherkovich Péter <uherkovich.peter@pte.hu>
2017-08-22 16:49:22 +0200

    composer directory case bug 2

--- ShowdownAsset.php
+++ ShowdownAsset.php
@@ -1,11 +1,11 @@
 <?php
-namespace uhi67\Showdown;
+namespace uhi67\showdown;
 
 class ShowdownAsset extends \yii\web\AssetBundle
 {
     // The files are not web directory accessible, therefore we need
     // to specify the sourcePath property.
-    public $sourcePath = '@vendor/uhi67/Showdown';
+    public $sourcePath = '@vendor/uhi67/showdown';
 
     public $css = [
         'Showdown.less',
